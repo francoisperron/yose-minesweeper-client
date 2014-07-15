84a7498adc3a75233d3fd8d600fbe2a1a711fc83
Francois Perron <fperron@gmail.com>
2014-07-14 21:12:19 -0400

    fix libs version

--- board.js
+++ board.js
@@ -1,11 +1,17 @@
+var $ = $ || require('jquery');
+
 function Board() {
 }
 
 Board.prototype.render = function () {
+    var board = $('#board');
     for (var i = 1; i < 9; i++) {
-        $('#board').append('<tr></tr>');
+        board.append('<tr></tr>');
         for (var j = 1; j < 9; j++) {
-            $('#board tr:last').append('<td id="cell-'+i+'x'+j+'"></td>');
+            board.find('tr:last').append('<td id="cell-'+i+'x'+j+'"></td>');
         }
     }
 };
+
+var module = module || {};
+module.exports = Board;
